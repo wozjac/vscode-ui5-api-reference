75ac9fc02a8be533f39f7dc45831724c0a4a7426
Jacek W <wozjac@zoho.com>
2022-08-20 01:26:18 +0200

    test: add constructorFormat tests

---
+++ src/test/suite/unit/objectApi/constructorFormat.test.ts
@@ -0,0 +1,56 @@
+import { expect } from "chai";
+import * as stubber from "../../../support/stubber";
+import { prepareConstructor } from "../../../../objectApi/constructorFormat";
+import { Ui5ObjectConstructor, LibraryApiSymbol } from "../../../../core/types";
+import { ui5ObjectApi } from "./common";
+
+const rawConstructor: Ui5ObjectConstructor = {
+  description: "This is <code>constructor</code>",
+  parameters: [
+    {
+      description: "This is constructor <code>param</code>",
+      type: "string",
+      name: "myName",
+      optional: false,
+      parameterProperties: [
+        {
+          name: "property",
+          description: "Parameter <code>property</code>",
+        },
+      ],
+    },
+  ],
+};
+
+describe("constructorFormat tests", () => {
+  before(() => {
+    stubber.stubUI5Service();
+  });
+
+  after(() => {
+    stubber.restore();
+  });
+
+  it("should prepare basic fields with parameters", () => {
+    const formattedConstructor = prepareConstructor(rawConstructor, ui5ObjectApi, true);
+
+    expect(formattedConstructor).not.to.be.undefined;
+    expect(formattedConstructor.description).to.equal("This is constructor");
+    expect(formattedConstructor.parameters).to.have.lengthOf(1);
+
+    if (formattedConstructor.parameters) {
+      const parameter = formattedConstructor.parameters[0];
+      expect(parameter.description).to.equal("This is constructor param");
+      expect(parameter.objectName).to.equal("sap.m.ColumnListItem");
+      expect(parameter.hasProperties).to.be.true;
+      expect(parameter.parameterProperties).to.have.lengthOf(1);
+
+      if (parameter.parameterProperties) {
+        const paramProperty = parameter.parameterProperties[0];
+        expect(paramProperty.name).to.equal("property");
+        expect(paramProperty.description).to.equal("Parameter property");
+        expect(paramProperty.objectName).to.equal("sap.m.ColumnListItem");
+      }
+    }
+  });
+});

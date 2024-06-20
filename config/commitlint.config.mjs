455f90a19eb81bc27009ffc0a8bb3051df46db8e
Jacek W <wozjac@zoho.com>
2024-06-20 21:58:02 +0200

    build: commitlint config

--- config/commitlint.config.mjs
+++ config/commitlint.config.mjs
@@ -1,1 +1,6 @@
-export default { extends: ["@commitlint/config-conventional"] };
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "max-body-line-length": [2, "always", 120],
+  },
+};
